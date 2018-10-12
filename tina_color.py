5702c456bb9a7ff1f032d8097912a78cae837bc1
Motaz Saad <motaz.saad@gmail.com>
2018-10-12 22:27:50 +0300

    add tina examples

---
+++ tina_color.py
@@ -0,0 +1,22 @@
+import turtle
+
+tina = turtle.Turtle()
+tina.shape('turtle')
+
+tina.left(90)
+tina.forward(20)
+tina.write("What color am I now?")
+
+tina.forward(20)
+tina.color("blue")
+tina.write("What color am I now?")
+
+tina.forward(20)
+tina.color("purple")
+tina.write("What color am I now?")
+
+tina.forward(20)
+tina.color("green")
+tina.write("What color am I now?")
+
+input('press any key ...')
---
+++ tina_move.py
@@ -0,0 +1,16 @@
+import turtle
+
+tina = turtle.Turtle()
+tina.shape("turtle")
+
+tina.forward(50)
+input('press any key')
+tina.left(90)
+input('press any key')
+tina.forward(50)
+input('press any key')
+tina.left(90)
+input('press any key')
+tina.forward(50)
+
+input('press any key ...')
