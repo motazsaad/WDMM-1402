0ce2e09f3bfc007a24070efd8143171982263ed7
Motaz Saad <motaz.saad@gmail.com>
2018-11-03 07:25:04 +0200

    add average line and format printing

--- chapter5/loop_before_after.py
+++ chapter5/loop_before_after.py
@@ -1,10 +1,11 @@
 count = 0
 total = 0
-print('count (before)', count)
-print('total (before)', total)
+print('count (before):', count)
+print('total (before):', total)
 for element in [9, 41, 12, 3, 74, 15]:
     count = count + 1
     total = total + element
     print(count, element)
-print('count (after)', count)
-print('total (after)', total)
+print('count (after):', count)
+print('total (after):', total)
+print('average:', total / count)
