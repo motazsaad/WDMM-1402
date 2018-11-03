420ec3f5a89f89b19ae3c1c15b52500e825933b7
Motaz Saad <motaz.saad@gmail.com>
2018-11-03 07:06:13 +0200

    add some lines related to size (length) of a list

--- chapter5/black_list.py
+++ chapter5/black_list.py
@@ -1,4 +1,6 @@
 # list of strings 
 black_list = ['Ahmad', 'Ali', 'Adel']
+print('list:', black_list)
+print('size:', len(black_list))
 for name in black_list:
     print(name + ', you are in the black list!')
--- chapter5/friends.py
+++ chapter5/friends.py
@@ -1,5 +1,7 @@
 # a list of strings
 friends = ['Joseph', 'David', 'Sally']
+print('list:', friends)
+print('size:', len(friends))
 for friend in friends:
     print('Happy New Year:', friend)
 print('Done!')
