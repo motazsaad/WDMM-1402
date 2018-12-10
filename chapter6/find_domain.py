6cad4f31b85701f06ceca8a9faf3d770bdf58eef
Motaz Saad <motaz.saad@gmail.com>
2018-12-10 10:34:29 +0200

    add more string examples

---
+++ chapter6/find_domain.py
@@ -0,0 +1,1 @@
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
---
+++ chapter6/find_words_starts_consentants_ends_vowels.py
@@ -0,0 +1,20 @@
+'''
+اكتب برنامج لعد الكلمات في نص بحيث تبدأ بحرف m وتنتهي بحرف متحرك (a e i o u ) سواء كان الحرف كبير ام صغير
+
+'''
+
+text = ''' Hi , my name is Ahmed , and 
+I like banana , Orange , 
+and apple fruits - but I do not like 
+APPLE COMPANY !!!
+I love Python , and python is cool ! :)
+I AM A PYTHON PROGRAMMER ! OK 
+please share , comment #python ;)   
+'''
+count = 0
+vowels = 'aeiou'
+for word in text.split():
+    small_word = word.lower()
+    if small_word.startswith('a') and small_word[-1] in vowels:
+        count += 1
+print(count)
